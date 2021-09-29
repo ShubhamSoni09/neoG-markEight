349a098e5dcc2571126b10c28be667c2b7640b84
Shubham Soni <18bec110@nirmauni.ac.in>
2021-09-30 00:44:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Know your Fruit!
+
+[neoG mark8]
+
+<a href="https://markeight-fruit.netlify.app/"><img src="https://github.com/himadri2110/portfolio/blob/main/img/fruit.png" /></a>
+
+## Features
+
+- Enter a fruit emoji to know its name
+- Click on a fruit to know its name
+- Made with React.js
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import "./styles.css";
+import { useState } from "react";
+
+var fruitDictionary = {
+  "": "",
+  "🍎": "Apple",
+  "🍐": "Pear",
+  "🍉": "Watermelon",
+  "🍍": "Pineapple",
+  "🥝": "Kiwi",
+  "🥭": "Mango",
+  "🍌": "Banana",
+  "🍓": "Strawberry",
+  "🍇": "Grapes",
+  "🍏": "Green Apple",
+  "🍑": "Peach",
+  "🍒": "Cherry",
+  "🍈": "Melon"
+};
+
+var fruitKey = Object.keys(fruitDictionary);
+
+export default function App() {
+  var [input, setInput] = useState("");
+  var showOutput;
+
+  function inputChangeHandler(event) {
+    input = event.target.value;
+    showOutput = fruitDictionary[input];
+
+    if (!(input in fruitDictionary)) {
+      showOutput = "No such fruit found!";
+    }
+    setInput(showOutput);
+  }
+
+  function fruitClickHandler(fruit) {
+    showOutput = fruitDictionary[fruit];
+    setInput(showOutput);
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Know your Fruit!</h1>
+        <p className="github">
+          <a
+            href="https://github.com/ShubhamSoni09/fruit-emoji"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            GitHub Repo
+          </a>
+        </p>
+      </header>
+
+      <main>
+        <input
+          onChange={inputChangeHandler}
+          placeholder="Enter fruit emoji to know its name"
+        ></input>
+
+        <div className="output">{input}</div>
+
+        <div> Fruits we know: </div>
+
+        <div className="fruitDiv">
+          {fruitKey.map((fruit) => {
+            return (
+              <span onClick={() => fruitClickHandler(fruit)}>{fruit}</span>
+            );
+          })}
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://shubhamsoni.me/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Shubham Soni
+        </a>
+      </footer>
+    </div>
+  );
+}
